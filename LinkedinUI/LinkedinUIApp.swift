fe286e756191b6cff0c202e688051610b37ca4ba
Ammar Abbas <95864103+Amar2Kk@users.noreply.github.com>
2024-05-12 18:15:24 +0300

    Initial Commit

---
+++ LinkedinUI/LinkedinUIApp.swift
@@ -0,0 +1,17 @@
+//
+//  LinkedinUIApp.swift
+//  LinkedinUI
+//
+//  Created by Ammar Abbas on 12/05/2024.
+//
+
+import SwiftUI
+
+@main
+struct LinkedinUIApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
